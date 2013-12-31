f5f860e2c7ec699dacb1a532c827e73c5dbafc82
Jack <543413507@qq.com>
2013-12-31 23:12:50 +0000

    第一次完工上传项目

---
+++ Project/XHFriendlyLoadingView/ViewController.m
@@ -0,0 +1,74 @@
+//
+//  ViewController.m
+//  XHFriendlyLoadingView
+//
+//  Created by 曾 宪华 on 13-12-31.
+//  Copyright (c) 2013年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "XHFriendlyLoadingView.h"
+
+@interface ViewController () {
+    NSInteger selectedCount; // default is 1
+}
+
+@property (nonatomic, strong) XHFriendlyLoadingView *friendlyLoadingView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self showLoading];
+}
+
+- (void)showLoading {
+    [self.friendlyLoadingView showFriendlyLoadingViewWithText:@"正在加载..." loadingAnimated:YES];
+    
+    double delayInSeconds = 3.0;
+    __weak typeof(self) weakSelf = self;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void) {
+        selectedCount ++;
+        if (selectedCount == 3) {
+            [weakSelf.friendlyLoadingView showFriendlyLoadingViewWithText:@"重新加载失败，请返回检查网络。" loadingAnimated:NO];
+        } else {
+            [weakSelf.friendlyLoadingView showReloadViewWithText:@"加载失败，请点击刷新按钮重新加载。"];
+        }
+    });
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    _friendlyLoadingView = [[XHFriendlyLoadingView alloc] initWithFrame:self.view.bounds];
+    
+    __weak typeof(self) weakSelf = self;
+    self.friendlyLoadingView.reloadButtonClickedCompleted = ^(UIButton *sender) {
+        // 这里可以做网络重新加载的地方
+        
+        [weakSelf showLoading];
+    };
+    [self.view addSubview:self.friendlyLoadingView];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc {
+    self.friendlyLoadingView = nil;
+}
+
+@end
---
+++ Source/XHFriendlyLoadingView.m
@@ -0,0 +1,201 @@
+//
+//  XHFriendlyLoadingView.m
+//  XHFriendlyLoadingView
+//
+//  Created by 曾 宪华 on 13-12-31.
+//  Copyright (c) 2013年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507. All rights reserved.
+//
+
+#import "XHFriendlyLoadingView.h"
+
+@interface XHFriendlyLoadingView () {
+    
+}
+
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicatorView;
+@property (nonatomic, strong) UILabel *loadingLabel;
+
+@property (nonatomic, strong) UIButton *reloadButton;
+
+@end
+
+@implementation XHFriendlyLoadingView
+
+- (void)reloadButtonClicked:(UIButton *)sender {
+    __weak typeof(self) weakSelf = self;
+    if (self.reloadButtonClickedCompleted) {
+        self.reloadButtonClickedCompleted(weakSelf.reloadButton);
+    }
+}
+
+- (void)_setup {
+    self.backgroundColor = [UIColor colorWithRed:(244 / 255.f) green:(246 / 255.f) blue:(252 / 255.f) alpha:1.f];
+    
+    _activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    _activityIndicatorView.alpha = 0.;
+    
+    _loadingLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.frame), 44)];
+    self.loadingLabel.alpha = 0.;
+    self.loadingLabel.textColor = [UIColor grayColor];
+    
+    _reloadButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    self.reloadButton.alpha = 0.;
+    [self.reloadButton setBackgroundImage:[UIImage imageNamed:@"refresh"] forState:UIControlStateNormal];
+    [self.reloadButton addTarget:self action:@selector(reloadButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    [self addSubview:self.activityIndicatorView];
+    [self addSubview:self.loadingLabel];
+    [self addSubview:self.reloadButton];
+}
+
++ (instancetype)shareFriendlyLoadingView {
+    static XHFriendlyLoadingView *instance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[XHFriendlyLoadingView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    });
+    return instance;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        [self _setup];
+    }
+    return self;
+}
+
+- (void)dealloc {
+    self.reloadButtonClickedCompleted = nil;
+    
+    [self _stopActivityIndicatorView];
+    self.activityIndicatorView = nil;
+    self.loadingLabel = nil;
+    self.reloadButton = nil;
+}
+
+- (CGRect)_setpLoadingLabelForWidth:(CGFloat)width {
+    CGRect loadingLabelFrame = self.loadingLabel.frame;
+    loadingLabelFrame.size.width = width;
+    loadingLabelFrame.origin.x = 0;
+    loadingLabelFrame.origin.y = CGRectGetHeight(self.bounds) / 2.0 - CGRectGetHeight(loadingLabelFrame) / 2.0;
+    return loadingLabelFrame;
+}
+
+- (void)_stopActivityIndicatorView {
+    if ([self.activityIndicatorView isAnimating]) {
+        [self.activityIndicatorView stopAnimating];
+    }
+}
+
+- (void)_setupAllUIWithAlpha {
+    self.loadingLabel.alpha = 0.;
+    self.reloadButton.alpha = 0.;
+    self.activityIndicatorView.alpha = 0.;
+}
+
+- (void)showFriendlyLoadingViewWithText:(NSString *)text loadingAnimated:(BOOL)animated {
+    [self _setupAllUIWithAlpha];
+    if (animated) {
+        [self showLoadingAnimationWithText:text];
+    } else {
+        [self showPromptViewWithText:text];
+    }
+}
+
+/**
+ * 纯文字提示
+ * @param promptString 要显示的提示字符串
+ */
+- (void)showPromptViewWithText:(NSString *)promptString {
+    [self _stopActivityIndicatorView];
+    
+    // 只是显示一行文本
+    self.loadingLabel.frame = [self _setpLoadingLabelForWidth:CGRectGetWidth(self.bounds)];
+    self.loadingLabel.textAlignment = NSTextAlignmentCenter;
+    self.loadingLabel.text = promptString;
+    
+    [UIView animateWithDuration:0.1 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        self.activityIndicatorView.alpha = 0.;
+        self.reloadButton.alpha = 0.;
+        self.loadingLabel.alpha = 1.;
+    } completion:^(BOOL finished) {
+        
+    }];
+}
+
+/**
+ * 页面加载动画及信息提示
+ * @param loadingString 要显示的提示字符串
+ */
+- (void)showLoadingAnimationWithText:(NSString *)loadingString {
+    CGPoint activityIndicatorViewCentet = CGPointMake(CGRectGetWidth(self.bounds) / 2.0 * 2 / 2.8, CGRectGetHeight(self.bounds) / 2.0);
+    self.activityIndicatorView.center = activityIndicatorViewCentet;
+    [self.activityIndicatorView startAnimating];
+    
+    CGRect loadingLabelFrmae = [self _setpLoadingLabelForWidth:CGRectGetWidth(self.bounds) / 3];
+    self.loadingLabel.textAlignment = NSTextAlignmentLeft;
+    loadingLabelFrmae.origin.x = CGRectGetWidth(self.activityIndicatorView.frame) + self.activityIndicatorView.frame.origin.x + 8;
+    loadingLabelFrmae.origin.y = CGRectGetHeight(self.bounds) / 2.0 - CGRectGetHeight(loadingLabelFrmae) / 2.0;
+    self.loadingLabel.frame = loadingLabelFrmae;
+    self.loadingLabel.text = loadingString;
+
+    [UIView animateWithDuration:0.1 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        self.reloadButton.alpha = 0.;
+        self.loadingLabel.alpha = 1.;
+        self.activityIndicatorView.alpha = 1.;
+    } completion:^(BOOL finished) {
+        
+    }];
+}
+
+/**
+ * 隐藏页面加载动画及信息提示
+ */
+- (void)hideLoadingView {
+    [UIView animateWithDuration:0.1 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        self.alpha = 0.;
+    } completion:^(BOOL finished) {
+        [self removeFromSuperview];
+    }];
+}
+
+/**
+ * 重新加载提示
+ * @param reloadString 要显示的提示字符串
+ */
+- (void)showReloadViewWithText:(NSString *)reloadString {
+    [self _setupAllUIWithAlpha];
+    [self _stopActivityIndicatorView];
+    
+    CGRect loadingLabelFrame = [self _setpLoadingLabelForWidth:CGRectGetWidth(self.bounds)];
+    self.loadingLabel.textAlignment = NSTextAlignmentCenter;
+    self.loadingLabel.frame = loadingLabelFrame;
+    self.loadingLabel.text = reloadString;
+    
+    // 按钮和提示
+    CGPoint reloadButtonCenter = CGPointMake(CGRectGetWidth(self.bounds) / 2.0, self.loadingLabel.center.y - (CGRectGetHeight(self.loadingLabel.frame) / 2.0 + CGRectGetHeight(self.reloadButton.frame) / 2.0));
+    self.reloadButton.center = reloadButtonCenter;
+    
+    [UIView animateWithDuration:0.1 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        self.activityIndicatorView.alpha = 0.;
+        self.reloadButton.alpha = 1.;
+        self.loadingLabel.alpha = 1.;
+    } completion:^(BOOL finished) {
+        
+    }];
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
