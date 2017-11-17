4e36498c44ea1b7163bb03b53e20b21c1fd32f48
zhaopeng <nemozp@sina.cn>
2017-11-17 23:48:53 +0800

    说明修改。

--- Project/XHFriendlyLoadingView/ViewController.m
+++ Project/XHFriendlyLoadingView/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 /**
- * 先把某一项目Fork到自己的github网站上，然后打开Xcode，选择"Clone an existing project"，然后选择那个已经Fork到自己github网站上的项目（不要选择源项目），然后Xcode就会把此项目下载到本地，从而在本地建立一个该项目的本地仓库。对本地仓库里面的代码进行修改，修改完利用Xcode上面的"Source Control"里面的"Commit"把修改完的代码提交到本地仓库中，然后利用"Push"把修改完的代码提交到那个已经Fork到自己github网站上的项目上的远程仓库中，然后在自己的github网站上点击"Pull requests"进入到新的页面，这时就会跳到此项目的源头那里，在新的页面中点击绿色的按钮"New pull request"进入到新的页面，这时
+ * 先把某一项目Fork到自己的github网站上，然后打开Xcode，选择"Clone an existing project"，然后选择那个已经Fork到自己github网站上的项目（不要选择源项目），然后Xcode就会把此项目下载到本地，从而在本地建立一个该项目的本地仓库。对本地仓库里面的代码进行修改，修改完以后利用Xcode上面的"Source Control"里面的"Commit"把修改完的代码提交到本地仓库中，然后利用"Push"把修改完的代码提交到那个已经Fork到自己github网站上的项目所在的自己github网站上的远程仓库中，然后在自己的github网站上点击"Pull requests"进入到新的页面，这时就会跳到此项目的源头那里，在新的页面中点击绿色的按钮"New pull request"进入到新的页面，这时
  */
 
 
